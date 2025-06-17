2ef6d30843e11b422060d2a60e407dc29336b07b
mrodriguez <MRodriguez@canogaperkins.net>
2025-06-16 22:48:48 -0700

    create-home-page-carousel

--- lib/data.ts
+++ lib/data.ts
@@ -32,7 +32,37 @@ const data = {
             name: 'Help',
             href: '/page/help',
         },
-    ]
+    ],
+    carousels: [
+        {
+          title: 'Most Popular Exterior Cameras For Sale',
+          buttonCaption: 'Shop Now',
+          image: '/images/camaras-para-exterior-sitio-web.jpg',
+          url: '/search?category=Exterior Cameras',
+          isPublished: true,
+        },
+        {
+          title: 'Most Popular Screes For Sale',
+          buttonCaption: 'Shop Now',
+          image: '/images/pantala105-3000x540px.jpg',
+          url: '/search?category=Screens',
+          isPublished: true,
+        },
+        {
+          title: 'Security Sensors',
+          buttonCaption: 'See More',
+          image: '/images/sitio-web-min-moe.jpg',
+          url: '/search?category=Security Sensors',
+          isPublished: true,
+        },
+        {
+            title: 'Best Deals on Unifi 7',
+            buttonCaption: 'See More',
+            image: '/images/unifi7.jpg',
+            url: '/search?category=Unifi 7',
+            isPublished: true,
+          },
+      ],
 }
 
 export default data
