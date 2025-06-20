9e0d5425d57bb61586a8babaf20281eb519006e8
mrodriguez <MRodriguez@canogaperkins.net>
2025-06-20 10:01:28 -0700

    creae-product-details-page

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,3 +1,5 @@
 export const APP_NAME = process.env.NEXT_PUBLIC_APP_NAME || "RGR-ECOMMERCE";
 export const APP_SLOGAN = process.env.NEXT_PUBLIC_APP_SLOGAN || "Spend less, enjoy more";
 export const APP_DESCRIPTION = process.env.NEXT_PUBLIC_APP_DESCRIPTION || "An Amazon clone build with Next.js and MongoDB";
+
+export const PAGE_SIZE = Number(process.env.NEXT_PUBLIC_PAGE_SIZE) || 9;
